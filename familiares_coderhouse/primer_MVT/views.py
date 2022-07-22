3cc83581e3f6e81414cc5e2920c483f4cdf373fd
manoloacademia <luispablosegovia@icloud.com>
2022-07-22 11:39:54 -0300

    Se puebla la base de datos con mis familiares.

--- familiares_coderhouse/primer_MVT/views.py
+++ familiares_coderhouse/primer_MVT/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Familiar
 
 # Create your views here.
 def home(request):
-    context = {}
+    familiares = Familiar.objects.all()
+    context = {'familiares': familiares}
     return render(request, 'home.html', context=context)
