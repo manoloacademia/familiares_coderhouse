d9e1fcd6f8960a5fd4463478e1d3aca105fefa55
manoloacademia <luispablosegovia@icloud.com>
2022-07-22 11:12:22 -0300

    Creada carpeta de templates. Añadida app al settings. Creada ruta de templates en settings.

--- familiares_coderhouse/familiares_coderhouse/settings.py
+++ familiares_coderhouse/familiares_coderhouse/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'primer_MVT',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'familiares_coderhouse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
